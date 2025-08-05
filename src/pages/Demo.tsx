c3cf42d4844dbdfd114e20fb452554589ea0af87
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-08-05 11:36:34 -0400

    demo video

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -4,6 +4,22 @@ export default function Demo() {
   return (
     <div className="demo-page">
       <h1>Aria Glasses Demo</h1>
+
+      {/* YouTube Video Embed */}
+      <div style={{ marginBottom: "2rem" }}>
+        <iframe
+          width="100%"
+          height="450"
+          src="https://www.youtube.com/embed/_ICmwEBYVN8"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ borderRadius: "12px", maxWidth: "800px" }}
+        ></iframe>
+      </div>
+
+      {/* Local Video */}
       <video
         width="100%"
         height="auto"
