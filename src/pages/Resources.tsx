80ba93e846d4b66fec9115ae01a446bda0b46dd5
arshiagarg2006 <arshiagarg2006@gmail.com>
2025-08-10 13:58:41 -0400

    resources

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -3,8 +3,9 @@ function Resources() {
       <div>
         <h2>Resources</h2>
         <ul>
-          <li><a href="https://facebookresearch.github.io/projectaria_tools/">Meta Project Aria documentation</a></li>
-          <li><a href="https://github.com/arshiagarg2006/winlab-glasses">Our GitHub Repo</a></li>
+          <li><a href="https://facebookresearch.github.io/projectaria_tools/docs/intro">Meta Project Aria documentation</a></li>
+          <li><a href="https://gitlab.orbit-lab.org/neilsam19/winlab-summer-2025-visual-perception-ar-glasses">Research Project GitLab Repo</a></li>
+          <li><a href="https://github.com/arshiagarg2006/winlab-glasses">Website GitHub Repo</a></li>
         </ul>
       </div>
     );
